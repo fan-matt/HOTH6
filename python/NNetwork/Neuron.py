86a054b04f1e821343acc875859839828bf41936
Matt Fan <fanma@ucla.edu>
2019-02-17 10:55:40 -0800

    Neuron method documentation

--- python/NNetwork/Neuron.py
+++ python/NNetwork/Neuron.py
@@ -2,6 +2,8 @@ import random
 
 
 class Neuron:
+    # Sets bias and activation to 0
+    # No connections
     def __init__(self):
         self.bias = 0
         self.activation = 0
@@ -10,19 +12,26 @@ class Neuron:
         # Just use them in order, from top to bottom
     
 
+    # Takes raw input and bias and pipes it through a sigmoid
+    # Also sets the activation to the output of the sigmoid
     def rawInput(self , input):
         self.activation = self.sigmoid(input - self.bias)
 
 
+    # Standard sigmoid function
     def sigmoid(self , input):
         e = 2.71828
         return (1 / (1 + e ** -input))
 
 
+    # Adds a connection to the neuron. A connection is just the value
+    # of a bias- the neuron doesn't need to know what comes before
+    # or after it
     def addConnection(self):
         self.connections.append(random.uniform(-1 , 1))
 
 
+    # Prints out all the properties of the neuron
     def print(self):
         print("-- Neuron -- ")
         print("Activation: " + str(self.activation))
